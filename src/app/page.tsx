3300a3c85baf45b9cfc2348ddeb58f7396a666e6
Khushal Patil <khushalpatil3055@gmail.com>
2026-03-20 15:38:31 +0530

    fix home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-2xl font-semibold mb-4">
+        Welcome to Score Tracker
+      </h1>
 
-      <Link href="/login">
-        <button>Login</button>
+      <Link
+        href="/login"
+        className="bg-blue-600 text-white px-4 py-2 rounded-lg"
+      >
+        Go to Login
       </Link>
     </div>
   );
